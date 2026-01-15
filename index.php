225c0be1bca2297c1bf3b7669347cd67be0c6022
heimerzieher <heimerzieher@me.com>
2026-01-15 16:20:05 +0100

    Updates

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 
 <div class="d-flex">
     <div class="flex-grow-1 ms-3">
-        <p>I am a PhD at student at <a href="https://www.uwo.ca">Western University</a> interested in (higher) category theory, logic and type theory and homotopical algebra. My supervisors are Chris Kapulkin (Department of Mathematics) and John Bell (Department of Philosophy). I obtained a MSc in Mathematics from Western and an MA in Philosophy from <a href="https://www.usi.ch/en/education/master/philosophy">Università della Svizzera italiana, Lugano, Switzerland</a>.</p>
-    
-        <p>Currently, I am working on the interpretation and semantics of homotopy type theory (HoTT) in infinity-categories. Part of my research is developing a semantic framework for HoTT in elementary infinity toposes.</p>
+        <div class="col-md-6">
+            <img src="img/picture.jpg" class="card-img-top" style="max-width: 150px;">
+            <h3 class="text-center">Maximilian Petrowitsch</h3>
+        </div>
+        <div class="col-md-6">
+            <p>I am a PhD at student at <a href="https://www.uwo.ca">Western University</a> interested in (higher) category theory, logic and type theory and homotopical algebra. My supervisors are Chris Kapulkin (Department of Mathematics) and John Bell (Department of Philosophy). I obtained a MSc in Mathematics from Western and an MA in Philosophy from <a href="https://www.usi.ch/en/education/master/philosophy">Università della Svizzera italiana, Lugano, Switzerland</a>.</p>
+        
+            <p>Currently, I am working on the interpretation and semantics of homotopy type theory (HoTT) in infinity-categories. Part of my research is developing a semantic framework for HoTT in elementary infinity toposes.</p>
+        </div>
+        <!-- <div class="container" id="pictureContainer">
+            <div class="card">
+                <img src="img/picture.jpg" class="card-img-top" style="max-width: 150px;">
+                <div class="card-body">
+                    <p class="card-text">Maximilian Petrowitsch</p>
+                </div>
+            </div>
+        </div> -->
     </div>
 </div>
 
