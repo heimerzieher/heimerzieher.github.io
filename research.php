78c6aaf2426aa1f6b0b8ad87093fc2a86d978d2b
heimerzieher <heimerzieher@me.com>
2026-01-17 23:15:57 +0100

    Added google meta key

--- research.php
+++ research.php
@@ -1,24 +1,24 @@
 <?php $selected = 'reserach'; include 'header.php';?>
 
-<h4>Preprints</h4>
+            <h4>Preprints</h4>
 
-<p>Elementary Infinity-Toposes from Type Theory, <a href="https://arxiv.org/abs/2512.18891">arxiv: 2512.18891</a></p>
+            <p>Elementary Infinity-Toposes from Type Theory, <a href="https://arxiv.org/abs/2512.18891">arxiv: 2512.18891</a></p>
 
-<br>
+            <br>
 
-<h4>Talks</h4>
+            <h4>Talks</h4>
 
-<p>Elementary Infinity-Toposes from Type Theory, CMS Summer Meeting, Session: Category Theory: Structures and Applications, Université Laval, Quebec City, June 8, 2025</p>
+            <p>Elementary Infinity-Toposes from Type Theory, CMS Summer Meeting, Session: Category Theory: Structures and Applications, Université Laval, Quebec City, June 8, 2025</p>
 
-<p>Elementary Infinity-Toposes from Type Theory, 32nd Foundational Methods in Computer Science Workshop, University of Ottawa, Ottawa, June 20, 2025</p>
+            <p>Elementary Infinity-Toposes from Type Theory, 32nd Foundational Methods in Computer Science Workshop, University of Ottawa, Ottawa, June 20, 2025</p>
 
-<p>Nominalism and Infinite Cardinality, ic.SoAP/EENPS, Belgrade and Geneva/Lugano (Online), 2021</p>
+            <p>Nominalism and Infinite Cardinality, ic.SoAP/EENPS, Belgrade and Geneva/Lugano (Online), 2021</p>
 
-<br>
+            <br>
 
-<h4>Formalisation projects</h4>
+            <h4>Formalisation projects</h4>
 
-<p>Integers as Higher Inductive Types, Formalisation of (Altenkirch and Scoccola 2020) in Coq, <a href="https://github.com/heimerzieher/Coq-HoTT/tree/integers_hit">repo</a></p>
+            <p>Integers as Higher Inductive Types, Formalisation of (Altenkirch and Scoccola 2020) in Coq, <a href="https://github.com/heimerzieher/Coq-HoTT/tree/integers_hit">repo</a></p>
 
 
 <?php include 'footer.php'; ?>
